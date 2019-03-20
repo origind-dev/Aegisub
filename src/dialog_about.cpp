7582e697449b2113e92e9ad2631b3b0c4b110a68
Charlie Jiang <cqjjjzr@126.com>
2019-03-20 23:25:07 +0800

    Bump about dialog.

--- src/dialog_about.cpp
+++ src/dialog_about.cpp
@@ -49,10 +49,11 @@ void ShowAboutDialog(wxWindow *parent) {
 
 	// Generate about string
 	wxString aboutString = wxString("Aegisub ") + GetAegisubShortVersionString() + ".\n"
-		"Copyright (c) 2005-2014 Rodrigo Braz Monteiro, Niels Martin Hansen, Thomas Goyne et al.\n\n"
+		"Copyright (c) 2005-2019 Rodrigo Braz Monteiro, Niels Martin Hansen, Thomas Goyne et al.\n\n"
 		"Programmers:\n"
 		"    Alysson Souza e Silva\n"
 		"    Amar Takhar\n"
+		"    Charlie Jiang\n"
 		"    Dan Donovan\n"
 		"    Daniel Moscoviter\n"
 		"    David Conrad\n"
